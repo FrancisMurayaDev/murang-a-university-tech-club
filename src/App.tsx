8b07ae5e8d93bed9c9af431f0ec8842f5f244143
FrancisMurayaDev <francismurayadev@gmail.com>
2025-03-23 01:00:34 +0300

    set navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import Home from "./Pages/Home"
 import Events from "./Pages/Events"
 import Leadership from "./Pages/Leadership"
 import Tracks from "./Pages/Tracks"
+import Navbar from "./Components/Navbar/Navbar"
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/leadership" element={<Leadership />}/>
