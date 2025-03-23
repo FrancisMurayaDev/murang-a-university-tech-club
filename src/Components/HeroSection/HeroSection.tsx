27d2928cc814464b5926954d3e7303d0266c582d
FrancisMurayaDev <francismurayadev@gmail.com>
2025-03-23 10:18:28 +0300

    hero

--- src/Components/HeroSection/HeroSection.tsx
+++ src/Components/HeroSection/HeroSection.tsx
@@ -1,9 +1,28 @@
-function HeroSection() {
-  return (
-    <div>
-      HeroSection
-      
-    </div>
+import "./HeroSection.css"
+import backgroundimage from "../../assets/hero.bg.jpg"
+
+
+function HeroSection({title, subtitle, backgroundimage}: HeroProps) {
+
+  type HeroProps = {
+    title: string;
+    subtitle: string;
+    backgroundimage: string
+  }
+  return ( 
+
+    
+    <section
+      className="hero"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <div className="overlay">
+        <h1>{title}</h1>
+        <p>{subtitle}</p>
+      </div>
+    </section>
+
+
   )
 }
 
